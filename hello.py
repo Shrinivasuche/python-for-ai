7ca2419c7c6217768fbec15f7d7a1bdb9eaed056
Shrinivasuche <shrinivasuche@gmail.com>
2026-03-23 18:51:22 +0530

    added and used Ruff formatter and linter

--- hello.py
+++ hello.py
@@ -1,16 +1,15 @@
-# Basic Python script to demonstrate API calls
-import requests
+import os
 
-response = requests.get('https://api.github.com')
+import requests
+from dotenv import load_dotenv
+
+# Basic Python script to demonstrate API calls
+response = requests.get("https://api.github.com")
 print(response.status_code)
 
 
 # setting environment variables
-import os
-from dotenv import load_dotenv
+load_dotenv()
 
-load_dotenv()  # Load variables from .env file
-
-#Read from environment 
-api_key = os.environ.get('API_KEY')
-database = os.environ.get('DATABASE_URL')
+api_key = os.environ.get("API_KEY")
+database = os.environ.get("DATABASE_URL")
