d97ea5e8b4a5fc4d5176deac39ff978ef0cc1c8d
Shrinivasuche <shrinivasuche@gmail.com>
2026-03-23 18:26:01 +0530

    using environment variables and secrets via .env files

--- hello.py
+++ hello.py
@@ -1,4 +1,16 @@
+# Basic Python script to demonstrate API calls
 import requests
 
 response = requests.get('https://api.github.com')
 print(response.status_code)
+
+
+# setting environment variables
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Load variables from .env file
+
+#Read from environment 
+api_key = os.environ.get('API_KEY')
+database = os.environ.get('DATABASE_URL')
